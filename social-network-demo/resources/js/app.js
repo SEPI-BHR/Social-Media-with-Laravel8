54fe9cc79958f507bf8bad38e2fb1f5a3ac0f03b
zammagoude <s.hemmati99@gmail.com>
2021-06-24 18:34:46 +0430

    Edit side bar and add Icons

--- social-network-demo/resources/js/app.js
+++ social-network-demo/resources/js/app.js
@@ -1,9 +1,12 @@
 require('./bootstrap');
 
 // Import modules...
+
 import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+
+
 
 const el = document.getElementById('app');
 
@@ -14,8 +17,10 @@ createApp({
             resolveComponent: (name) => require(`./Pages/${name}`).default,
         }),
 })
+    .component('icon', require('./Components/Icon').default)
     .mixin({ methods: { route } })
     .use(InertiaPlugin)
     .mount(el);
 
 InertiaProgress.init({ color: '#4B5563' });
+
